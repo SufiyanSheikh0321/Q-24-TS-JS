a4ff6035b00b7dffce177bd4b8decaf5777eae47
Muhammed Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-02-29 20:14:46 +0500

    Add files via upload

---
+++ Q.24.js
@@ -0,0 +1,42 @@
+"use strict";
+// 24. More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+// • Tests for equality and inequality with strings
+// • Tests using the lower case function
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+// • Tests using "and" and "or" operators
+// • Test whether an item is in a array
+// • Test whether an item is not in a array
+// Test 1
+// • Tests for equality and inequality with strings
+const string1 = "Mobile";
+const string2 = "Laptop";
+console.log(string1 === string2); //False strings Are not equal
+console.log(string1 !== string2);
+//Test 2
+// • Tests using the lower case function
+const mixedCaseString = "HelloWorld";
+console.log(mixedCaseString.toLowerCase() === "helloworld");
+//Test 3
+//  • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal 
+const num1 = 15;
+const num2 = 10;
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+//Test 4
+// • Tests using "and" and "or" operators
+const condition1 = true;
+const condition2 = false;
+console.log(condition1 && condition2);
+console.log(condition1 || condition2);
+//Test 5
+// • Test whether an item is in a array
+const Cars = ["Audi", "Honda City", "Toyota Yaris", "Fortuner"];
+console.log(Cars.includes("Audi"));
+//Test 6
+// • Test whether an item is not in a array
+const Cars1 = ["Audi", "Honda City", "Toyota Yaris", "Fortuner"];
+console.log(Cars1.includes("audi"));
